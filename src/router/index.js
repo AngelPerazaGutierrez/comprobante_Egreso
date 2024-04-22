03b47eeadeac687e832b8faaf24fdaedbe3da4e1
Angel <angel.peraza@peaku.io>
2024-04-22 17:19:02 -0500

    añadiendo en home

--- src/router/index.js
+++ src/router/index.js
@@ -23,14 +23,6 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "crear_egreso" */ '../views/CrearEgresoView.vue')
-  },
-  {
-    path: '/ver_egresos',
-    name: 'ver_egresos',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "ver_egreso" */ '../views/VerEgresosView.vue')
   }
   
 
